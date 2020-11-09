744f10202f58b69494ebe343c0bf31461dcfc1e4
Maulik Fojdar <maulik712@gmail.com>
2020-11-08 17:30:20 -0800

    Async API with hooks and effects

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Coctaise",
-    }),
-    React.createElement(Pet, {
-      name: "Bruno",
-      animal: "Dog",
-      breed: "Golden Retriever",
-    })
-  );
+
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  )
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
+import Results from './Results';
+import useDropdown from './useDropdown';
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({breeds}) => {
+      const breedStrings = breeds.map(({name}) => name);
+      setBreeds(breedStrings);
+    }, console.error)
+  }, [animal, setBreed, setBreeds])
+
+  return (
+    <div className="search-params">
+      <form onSubmit={e => {
+        e.preventDefault();
+        requestPets();
+      }}>
+        <label htmlFor="location">
+          Location
+          <input id="location" value={location}
+            onChange={e => setLocation(e.target.value)}
+            placeholder="Location" />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  )
+}
+
+export default SearchParams;
